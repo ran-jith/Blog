f40a9721bfd905ad1c9c3ce85f3ee3509a3c74a1
ranjithakalur <ranjithakalurkk@gmail.com>
2018-07-07 15:05:06 +0530

    cust filter usng markdown

---
+++ README.md
@@ -0,0 +1,4 @@
+install apps
+blog ==>created
+taggit==>downloaded from pip
+Markdown==>downloaded from pip
--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,6 +1,7 @@
 from django import template
 from django.db.models import Count
-
+from django.utils.safestring import mark_safe
+import markdown #custom filter
 #create a simple tag to retrieve total posts published in the blog
 register = template.Library()
 
@@ -21,3 +22,8 @@ def show_latest_posts(count=5):
 @register.assignment_tag
 def get_most_commented_posts(count=5):
     return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
+
+#custom filter
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
